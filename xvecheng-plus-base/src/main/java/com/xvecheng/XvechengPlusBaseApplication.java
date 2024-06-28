15f9e42d46f8b512cee9326784cfa0974c67a881
yaoyaole <350122500@qq.com>
2024-06-28 15:33:51 +0800

    提交付工程文件

---
+++ xvecheng-plus-base/src/main/java/com/xvecheng/XvechengPlusBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xvecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XvechengPlusBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XvechengPlusBaseApplication.class, args);
+    }
+
+}
---
+++ xvecheng-plus-base/src/test/java/com/xvecheng/XvechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xvecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XvechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
